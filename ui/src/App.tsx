910de6c17f7bf5d97ee516b5c1a351190e9d2f3c
ogioldat <tomasz.ogiolda00@gmail.com>
2024-04-07 00:23:31 +0200

    feat: Format project

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,19 +1,15 @@
-import './App.css'
-import { useWasm } from './use-wasm'
-import ImageEditor from './ImageEditor'
+import './App.css';
+import { useWasm } from './use-wasm';
+import ImageEditor from './ImageEditor';
 
 function App() {
-  const wasm = useWasm()
+  const wasm = useWasm();
   return (
     <>
-      <div>
-        Edit your image
-      </div>
-      {
-        !!wasm ? <ImageEditor wasm={wasm} /> : <div>Loading wasm</div>
-      }
+      <div>Edit your image</div>
+      {wasm ? <ImageEditor wasm={wasm} /> : <div>Loading wasm</div>}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- ui/src/use-wasm.ts
+++ ui/src/use-wasm.ts
@@ -1,17 +1,17 @@
-import type { InitOutput } from '@wasm'
+import type { InitOutput } from '@wasm';
 import { useEffect, useState } from 'react';
-import initWasm from '../../pkg'
+import initWasm from '../../pkg';
 
 export function useWasm(): InitOutput | null {
-    const [wasmModule, setWasmModule] = useState<InitOutput | null>(null);
+  const [wasmModule, setWasmModule] = useState<InitOutput | null>(null);
 
-    useEffect(() => {
-        (async (): Promise<InitOutput> => {
-            const mod = await initWasm()
-            setWasmModule(mod)
-            return mod
-        })()
-    }, []);
+  useEffect(() => {
+    (async (): Promise<InitOutput> => {
+      const mod = await initWasm();
+      setWasmModule(mod);
+      return mod;
+    })();
+  }, []);
 
-    return wasmModule;
+  return wasmModule;
 }
